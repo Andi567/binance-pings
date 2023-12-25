77d5c66efaef877929563aeaa8618dfc58ffe742
andipush <andi567314478@gmail.com>
2023-12-25 14:49:40 +0300

    Add ping functions

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,42 @@
-import { WebsocketAPI } from "@binance/connector-typescript";
+import { Spot, WebsocketAPI } from "@binance/connector";
 
-const API_KEY = "";
-const API_SECRET = "";
+const retries = 20;
 
-const callbacks = {
-  open: (client: WebsocketAPI) => client.exchangeInfo(),
-  close: () => console.debug("Disconnected from WebSocket server"),
-  message: (data: string) => console.info(JSON.parse(data)),
+const pingSpot = async (): Promise<number> => {
+  const client = new Spot();
+  const delayArr = [];
+
+  for (let i = 0; i < retries; i++) {
+    const t0 = performance.now();
+    const pingResponse = await client.ping();
+
+    if (pingResponse.status === 200 && i !== 0)
+      delayArr.push(performance.now() - t0);
+  }
+
+  return delayArr.reduce((a, b) => a + b, 0) / delayArr.length;
 };
 
-const websocketAPIClient = new WebsocketAPI(API_KEY, API_SECRET, { callbacks });
-setTimeout(() => websocketAPIClient.disconnect(), 20000);
+const pingWs = async () => {
+  // WebsocketAPI
+  let t0: number;
+  const callbacks = {
+    open: (client) => {
+      console.debug("Connected with Websocket server");
+      t0 = performance.now();
+      client.ping({ id: 1 });
+    },
+    close: () => console.debug("Disconnected with Websocket server"),
+    message: () => console.log("WebsocketAPI", performance.now() - t0, "ms"),
+  };
+
+  const websocketAPIClient = new WebsocketAPI(null, null, { callbacks });
+  setTimeout(() => websocketAPIClient.disconnect(), 10000);
+};
+
+(async function main() {
+  const spotAvgDelayInMs = await pingSpot();
+  console.log("Spot", spotAvgDelayInMs, "ms");
+
+  await pingWs();
+})();
